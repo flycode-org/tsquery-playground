c099e1161e46fdcf84b09e96272e950dc61eccd7
Iddan Aaronsohn <mail@aniddan.com>
2021-11-06 03:06:12 +0200

    Fix build

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,9 @@
 module.exports = function override(config, env) {
   // esquery exports a broken ESModule. This makes sure it will be imported correctly
   config.resolve.mainFields = ["main"];
+  // Do not parse typescript when bundling code
+  config.module.rules.noParse = [
+    require.resolve("typescript/lib/typescript.js"),
+  ];
   return config;
 };
