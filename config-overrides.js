fbe7b7df5c7c3fe2d6ccd47c437f0b3dde4ec026
Iddan Aaronsohn <mail@aniddan.com>
2021-11-28 00:18:41 +0200

    Supress build warnings

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,19 @@
+const webpack = require('webpack');
+
 module.exports = function override(config, env) {
   // esquery exports a broken ESModule. This makes sure it will be imported correctly
-  config.resolve.mainFields = ["main"];
+  config.resolve.mainFields = ['main'];
   // Do not parse typescript when bundling code
-  config.module.rules.noParse = [
-    require.resolve("typescript/lib/typescript.js"),
-  ];
+  config.module.rules.noParse = [require.resolve('typescript/lib/typescript.js')];
+  // Ignore errors raised when bundling typescript
+  config.plugins.push(
+    new webpack.ContextReplacementPlugin(/\/typescript\//, (data) => {
+      for (const dependency of data.dependencies) {
+        delete dependency.critical;
+      }
+      return data;
+    }),
+    new webpack.IgnorePlugin(/perf_hooks/),
+  );
   return config;
 };
